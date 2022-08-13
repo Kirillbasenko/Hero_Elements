d4e9f1bf64ed2371b38731c56bd986091e1258e1
Kirillbasenko <95930608+Kirillbasenko@users.noreply.github.com>
2022-08-13 11:24:54 +0300

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import {configureStore} from "@reduxjs/toolkit"
+import heroes from '..//components/heroesList/heroesSlice';
+import filters from '../components/heroesFilters/filtersSlice';
+
+   const stringMiddleware = ({dispatch, getState}) => (dispatch) => (action) => {
+      if(typeof action === "string"){
+            return dispatch({
+               type: action
+            })
+         }
+         return dispatch(action)
+   }
+
+   const store = configureStore({
+      reducer: {heroes, filters},
+      middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware),
+      devTools: process.env.NODE_ENV !== "production",
+   }) 
+
+export default store;
